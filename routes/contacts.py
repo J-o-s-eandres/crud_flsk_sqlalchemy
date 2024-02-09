4545d306c4d150f705e0837688f291e34ce413c4
Joseandres <joseandresmontesino@gmail.com>
2024-02-09 13:04:50 -0500

    new routes

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,13 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 conatcts = Blueprint('contacts', __name__)
 
-
 @conatcts.route('/')
 def home():
-    return "Contacts List"
-
+    return render_template("index.html")
 
 @conatcts.route('/new')
 def add_contact():
     return "saving a contact"
+
+@conatcts.route('/update')
+def update_contact():
+    return "update a contact"
+
+@conatcts.route('/delete')
+def delete_contact():
+    return "delete a contact"
+
+@conatcts.route('/about')
+def about():
+    return render_template("about.html")
