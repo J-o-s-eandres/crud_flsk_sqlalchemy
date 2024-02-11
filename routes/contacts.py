9555bc526a9112bb70ae3176947227ade5cfc1e5
Joseandres <joseandresmontesino@gmail.com>
2024-02-11 11:45:53 -0500

    delete contact

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template,request, redirect
+from flask import Blueprint, render_template,request, redirect,url_for
 from models.Contact import Contact
 from utils.db import db
 
 contacts = Blueprint('contacts', __name__)
 
 @contacts.route('/')
-def home():
+def index():
     contacts = Contact.query.all()
     return render_template("index.html", contacts=contacts)
 
@@ -17,15 +17,19 @@ def add_contact():
     new_contact = Contact(fullname, email, phone)
     db.session.add(new_contact)
     db.session.commit()
-    return redirect('/')
+    return redirect(url_for('contacts.index'))
 
 @contacts.route('/update')
 def update_contact():
     return "update a contact"
 
-@contacts.route('/delete')
-def delete_contact():
-    return "delete a contact"
+@contacts.route('/delete/<int:id>')
+def delete_contact(id):
+
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
+    return redirect(url_for('contacts.index'))
 
 @contacts.route('/about')
 def about():
