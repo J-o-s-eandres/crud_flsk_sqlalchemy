d03c95a03116ee7bf0b45f391ff0138e2b2d63fd
Joseandres <joseandresmontesino@gmail.com>
2024-02-11 11:30:58 -0500

    created new contact

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request, redirect
+from models.Contact import Contact
+from utils.db import db
 
 contacts = Blueprint('contacts', __name__)
 
@@ -6,9 +8,15 @@ contacts = Blueprint('contacts', __name__)
 def home():
     return render_template("index.html")
 
-@contacts.route('/new')
+@contacts.route('/new', methods=['POST'])
 def add_contact():
-    return "saving a contact"
+    fullname=request.form['fullname']
+    email=request.form['email']
+    phone=request.form['phone']
+    new_contact = Contact(fullname, email, phone)
+    db.session.add(new_contact)
+    db.session.commit()
+    return redirect('/')
 
 @contacts.route('/update')
 def update_contact():
